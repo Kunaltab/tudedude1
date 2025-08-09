a3a1556637f4f605a480ce0484ef258d06a3c9e9
prajapati kunal <prajapatikunal8763@gmail.com>
2025-08-09 12:17:28 +0530

    task3

---
+++ task3/task2.py
@@ -0,0 +1,8 @@
+import math
+num = float(input("enter a number"))
+sqrt = math.sqrt(num)
+log = math.log(num)
+sine = math.sine(num)
+print("square root :",sqrt)
+print("logarithm:",log)
+print ("sine:",sine)
