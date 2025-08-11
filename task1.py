c946741051bd5533757b66e4b5f006e5e929a9a8
prajapati kunal <prajapatikunal8763@gmail.com>
2025-08-11 17:17:22 +0530

    task5

--- task1.py
+++
@@ -1,7 +0,0 @@
-student_marks={"Alice":90}
-name = input("Enter the student name :")
-if name in student_marks:
-    print(f"{name}'s marks:{student_marks[name]}")
-else:
-    print ("student not found ")
- 
